e1124e43800f0f4491b4441133811f95c4900bd1
joewinnn <josephkimnguyen@gmail.com>
2016-09-24 02:11:30 -0400

    Initial set up for Watch and Phone. Currently is a counter that can save number from Watch app onto Parent App

--- HackGT2016/HackGT2016 WatchKit Extension/InterfaceController.swift
+++ HackGT2016/HackGT2016 WatchKit Extension/InterfaceController.swift
@@ -8,9 +8,48 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    @IBOutlet var counterLabel: WKInterfaceLabel!
+    var counter = 0
+    
+    var session : WCSession!
+    
+    @IBAction func incrementCounter() {
+        counter += 1
+        setCounterLabelText()
+    }
+    
+    @IBAction func clearCounter() {
+        counter = 0
+        setCounterLabelText()
+    }
+    
+    @IBAction func saveCounter() {
+        let applicationData = ["counterValue":String(counter)]
+        
+        session.sendMessage(applicationData, replyHandler: { (_: [String : Any]) in
+    
+            }) { (Error) in
+        }
+    }
+    
+    func setCounterLabelText() {
+        counterLabel.setText(String(counter))
+    }
+    
+    override func willActivate() {
+        super.willActivate()
+        
+        if (WCSession.isSupported()) {
+            session = WCSession.default()
+            session.delegate = self
+            session.activate()
+        }
+    }
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -18,14 +57,17 @@ class InterfaceController: WKInterfaceController {
         // Configure interface objects here.
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?){
+        
+    }
+    
+    
+    
+
 
 }
--- HackGT2016/HackGT2016/ViewController.swift
+++ HackGT2016/HackGT2016/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    var counterData = [String]()
+    var session: WCSession!
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?){
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession){
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession){
+        
+    }
+    
 
+
+
+
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (WCSession.isSupported()) {
+            session = WCSession.default()
+            session.delegate = self;
+            session.activate()
+        }
+        
+        self.counterLabel.reloadInputViews()
     }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        let counterValue = message["counterValue"] as? String
+        
+        //Use this to update the UI instantaneously (otherwise, takes a little while)
+        DispatchQueue.main.async {
+            self.counterLabel.text = counterValue!;
+            print(counterValue!)
+            self.counterLabel.reloadInputViews()
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
