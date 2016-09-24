d55fd48837934fb787ecbe915be34ccffdae6a9f
thkim1234 <tedkim1234@ufl.edu>
2016-09-24 02:56:43 -0400

    ViewController - Checking if the Watch is still connected, sending request messages between taps.

--- HackGT2016/HackGT2016/ViewController.swift
+++ HackGT2016/HackGT2016/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Sending a request message to watch between taps
+    //If the Watch is unavailable -> error.
+    @IBAction func backToWatchBwTap(_ sender: UIButton!){
+        if !WCSession.default().isReachable{
+            
+            let alert = UIAlertController(
+                title: "Send failed.",
+                message: "Apple Watch is unreachable.",
+                preferredStyle: UIAlertControllerStyle.alert)
+            let oK = UIAlertAction(
+                title: "OK",
+                style: UIAlertActionStyle.cancel,
+                handler: nil)
+            alert.addAction(oK)
+            present(alert, animated: true, completion: nil)
+            
+            return
+        }
+        
+        let msg = ["request": "showAlert"]
+        WCSession.default().sendMessage(
+            msg, replyHandler: { (replyMessage) -> Void in
+        }) { (error) -> Void in
+                print(error)
+        }
+        
+    }
+    
 
 }
 
