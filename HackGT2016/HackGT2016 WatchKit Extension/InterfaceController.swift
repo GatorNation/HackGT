d385b0eb068a0d9c59c23187089a64485a453e7e
David <dyeung93@hotmail.com>
2016-09-24 00:12:47 -0400

    added project

---
+++ HackGT2016/HackGT2016 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HackGT2016 WatchKit Extension
+//
+//  Created by David  Yeung on 9/24/16.
+//  Copyright © 2016 David Yeung. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
