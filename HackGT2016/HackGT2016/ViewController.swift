21515968a8fdbc100d54712ac54ab28dd067205c
Tom Guarnery <thomas.guarnery@gmail.com>
2016-09-24 14:17:50 -0400

    added text, userInfo, and userLocation support

--- HackGT2016/HackGT2016/ViewController.swift
+++ HackGT2016/HackGT2016/ViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 import WatchConnectivity
+import CoreLocation
 
-class ViewController: UIViewController, WCSessionDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate{
     
     @IBOutlet weak var counterLabel: UILabel!
     var counterData = [String]()
     var session: WCSession!
+    
+    var longitude = Double()
+    var latitude = Double()
+    
+    var locationManager = CLLocationManager()
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?){
         
@@ -26,7 +32,7 @@ class ViewController: UIViewController, WCSessionDelegate {
     func sessionDidDeactivate(_ session: WCSession){
         
     }
-    
+
 
 
 
@@ -39,8 +45,16 @@ class ViewController: UIViewController, WCSessionDelegate {
         
         if (WCSession.isSupported()) {
             session = WCSession.default()
-            session.delegate = self;
+//            session.delegate = self;
             session.activate()
+        }
+//            sendSMS()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+            if locationManager.location != nil {
+                latitude = locationManager.location!.coordinate.latitude
+                longitude = locationManager.location!.coordinate.longitude
+        print("Important. " + String(UIDevice.current.name) + " might be having a seizure at or around https://www.google.com/maps/@\(latitude),\(longitude),17z")
         }
         
         self.counterLabel.reloadInputViews()
@@ -56,6 +70,42 @@ class ViewController: UIViewController, WCSessionDelegate {
             self.counterLabel.reloadInputViews()
         }
 
+    }
+    func locateMe() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    func sendSMS()
+    {
+        
+        let twilioSID = "AC59be109d20772f0032a741c0453199a2"
+        let twilioSecret = "214e31ae411a750ead23cd6c8d1475de"
+        
+        //Note replace + = %2B , for To and From phone number
+//        let fromNumber = "%2B17656263107"// actual number is +14803606445
+        let fromNumber = "%217656263107"// actual number is +14803606445
+//        let toNumber = "%2B3522133462"// actual number is +919152346132
+//        let toNumber = "%2B9045132487"// actual number is +919152346132
+        let toNumber = "%2B9047181398"// actual number is +919152346132
+        let message = String(UIDevice.current.name) + " might be having a seizure at "
+        // Build the request
+        let request = NSMutableURLRequest(url: NSURL(string:"https://\(twilioSID):\(twilioSecret)@api.twilio.com/2010-04-01/Accounts/\(twilioSID)/SMS/Messages")! as URL)
+        request.httpMethod = "POST"
+        request.httpBody = "From=\(fromNumber)&To=\(toNumber)&Body=\(message)".data(using: String.Encoding.utf8)
+        
+        // Build the completion block and send the request
+        URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in
+            print("Finished")
+            if let data = data, let responseDetails = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+                // Success
+                print("Response: \(responseDetails)")
+            } else {
+                // Failure
+                print("Error: \(error)")
+            }
+        }).resume()
     }
     
 }
